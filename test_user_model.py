01e52b9cea6e78737ff4035a90c3491307e5062d
Daniel Bae <danibae91@icloud.com>
2021-06-13 18:08:37 -0700

    test_user_model

--- test_user_model.py
+++ test_user_model.py
@@ -1,9 +1,8 @@
 """User model tests."""
 
 # run these tests like:
-#
-#    python -m unittest test_user_model.py
-
+#   createdb warbler-test
+#   python -m unittest test_user_model.py
 
 import os
 from unittest import TestCase
@@ -17,7 +16,6 @@ from models import db, User, Message, Follows
 
 os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
 
-
 # Now we can import app
 
 from app import app
@@ -28,7 +26,6 @@ from app import app
 
 db.create_all()
 
-
 class UserModelTestCase(TestCase):
     """Test views for messages."""
 
@@ -38,6 +35,15 @@ class UserModelTestCase(TestCase):
         User.query.delete()
         Message.query.delete()
         Follows.query.delete()
+
+        # create user
+        self.user1 = User.signup(email='test1@test.com', username='testuser1', password='password', image_url=None)
+        self.user2 = User.signup(email='test2@test.com', username='testuser2', password='password', image_url=None)
+
+        # add to session & commit 
+        db.session.add(self.user1)
+        db.session.add(self.user2)
+        db.session.commit() 
 
         self.client = app.test_client()
 
@@ -53,6 +59,39 @@ class UserModelTestCase(TestCase):
         db.session.add(u)
         db.session.commit()
 
-        # User should have no messages & no followers
+        # User should have no messages & no followers 
+        # & no likes
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+        self.assertEqual(len(u.likes), 0)
+    
+    def test_repr(self):
+        user1 = repr(self.user1)
+        self.assertIn('test1', user1)
+        self.assertIn('testuser1', user1)
+    
+    def test_is_following(self):
+        # user1 follow user2
+        self.user1.following.append(self.user2)
+        db.session.commit() 
+
+        self.assertEqual(len(self.user2.following), 0)
+        self.assertEqual(len(self.user2.followers), 1)
+        self.assertEqual(len(self.user1.followers), 0)
+        self.assertEqual(len(self.user1.following), 1) 
+    
+    def test_signup(self): 
+        user = User.signup("test4", "test4@test.com", "password", None)
+        user.id = 952139000
+        db.session.commit()
+        testuser = User.query.get(user.id)
+        self.assertIsNotNone(testuser)
+        self.assertEqual(testuser.username, "test4")
+        self.assertEqual(testuser.email, "test4@test.com")
+        self.assertNotEqual(testuser.password, "password")
+
+    def test_user_authentication(self):
+        user = User.authenticate(self.user2.username, 'password')
+        self.assertEqual(user.id, self.user2.id)
+
+
